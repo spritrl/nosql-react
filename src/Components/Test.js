db6d930ebab40c72a63c7788fd158cdc8c9bd29a
Christophe <pro.chrisrln@gmail.com>
2022-04-27 23:26:38 +0200

    Chris: init components/views

---
+++ src/Components/Test.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const Test = () => {
+  return (
+    <a>Home View</a>
+  );
+};
+
+export default Test;
---
+++ src/Views/Home.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import Test from '../Components/Test';
+
+const HomeView = () => {
+  return (
+    <Test />
+  );
+};
+
+export default HomeView;
