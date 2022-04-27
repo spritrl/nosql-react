806a3c0a7dde1b4ef129852216088f53165ddd90
Christophe <pro.chrisrln@gmail.com>
2022-04-27 23:36:47 +0200

    Chris: My axios getter test

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,9 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Test from '../Components/Test';
+import axios from 'axios';
 
 const HomeView = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const getData = async () => {
+    console.warn('in getData');
+    await axios.get(`http://localhost:8080/api/`)
+      .then(res => {
+        const data = res.data;
+        return data;
+      })
+      .then(res => {
+        let arr = [];
+        res.forEach((element) => {
+          arr.push(element);
+        })
+        setFetchedData(arr);
+      })
+      .then(res => {
+        console.warn(fetchedData);
+      })
+  };
+
+  useEffect(() => {
+    getData().then(response => {
+      console.log(response);
+    }).catch(e => {
+      console.log(e);
+    });
+  }, []);
+
   return (
-    <Test />
+    <>
+      <Test />
+      <a>{fetchedData}</a>
+    </>
   );
 };
 
